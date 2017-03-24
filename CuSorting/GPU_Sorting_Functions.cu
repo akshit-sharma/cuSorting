08ca98e365dab5d706794d0b27482676c7ff2e4e
akshit-sharma <akshit9sharma@gmail.com>
2017-03-24 17:26:47 +0530

    Added empty func defination for gpus

--- CuSorting/GPU_Sorting_Functions.cu
+++ CuSorting/GPU_Sorting_Functions.cu
@@ -13,4 +13,45 @@ __global__ void odd_even_sort_results_paperid(PaperIdWrapper_Results * d_PaperId
 
 }
 
+__global__ void odd_even_sort_results_rollnumber(RollNumberWrapper_Results * d_results_rollnumberWrapper_Results)
+{
+	// Function body
+
+}
+
+__global__ void shellsort_scheme_paperid(PaperIdWrapper_Scheme * d_PaperIdWrapper_Scheme)
+{
+	// Function body
+
+}
+
+__global__ void shellsort_results_paperid(PaperIdWrapper_Results * d_PaperIdWrapper_Results)
+{
+	// Function body
+
+}
+
+__global__ void shellsort_results_rollnumber(RollNumberWrapper_Results * d_results_rollnumberWrapper_Results)
+{
+	// Function body
+
+}
+
+__global__ void quicksort_scheme_paperid(PaperIdWrapper_Scheme * d_PaperIdWrapper_Scheme)
+{
+	// Function body
+
+}
+
+__global__ void quicksort_results_paperid(PaperIdWrapper_Results * d_PaperIdWrapper_Results)
+{
+	// Function body
+
+}
+
+__global__ void quicksort_results_rollnumber(RollNumberWrapper_Results * d_results_rollnumberWrapper_Results)
+{
+	// Function body
+
+}
 
