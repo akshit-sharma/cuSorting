630de46ce6f8c98381f0c9df55ed13f76e5b1062
akshit-sharma <akshit9sharma@gmail.com>
2017-03-24 17:23:54 +0530

    Added func declaration for gpus

--- CuSorting/GPU_Sorting_Functions.cuh
+++ CuSorting/GPU_Sorting_Functions.cuh
@@ -4,7 +4,23 @@
 #include "cuRDStruct.h"
 #include "cuSDStruct.h"
 
-__global__ void odd_even_sort_scheme_paperid(PaperIdWrapper_Scheme * d_PaperIdWrapper);
-__global__ void odd_even_sort_results_paperid(PaperIdWrapper_Results * d_PaperIdWrapper);
+__global__ void odd_even_sort_scheme_instiname(InstitutionNameWrapper_Scheme * d_institutionNameWrapper_Scheme);
+__global__ void odd_even_sort_scheme_subjname(SubjectNameWrapper_Scheme * d_subjectNameWrapper_Scheme);
+__global__ void odd_even_sort_scheme_paperid(PaperIdWrapper_Scheme * d_PaperIdWrapper_Scheme);
+__global__ void odd_even_sort_results_paperid(PaperIdWrapper_Results * d_PaperIdWrapper_Results);
+__global__ void odd_even_sort_results_rollnumber(RollNumberWrapper_Results * d_results_rollnumberWrapper_Results);
+__global__ void odd_even_sort_results_name(NameWrapper_Results * d_nameWrapper_Results);
 
+__global__ void shellsort_scheme_instiname(InstitutionNameWrapper_Scheme * d_institutionNameWrapper_Scheme);
+__global__ void shellsort_scheme_subjname(SubjectNameWrapper_Scheme * d_subjectNameWrapper_Scheme);
+__global__ void shellsort_scheme_paperid(PaperIdWrapper_Scheme * d_PaperIdWrapper_Scheme);
+__global__ void shellsort_results_paperid(PaperIdWrapper_Results * d_PaperIdWrapper_Results);
+__global__ void shellsort_results_rollnumber(RollNumberWrapper_Results * d_results_rollnumberWrapper_Results);
+__global__ void shellsort_results_name(NameWrapper_Results * d_nameWrapper_Results);
 
+__global__ void quicksort_scheme_instiname(InstitutionNameWrapper_Scheme * d_institutionNameWrapper_Scheme);
+__global__ void quicksort_scheme_subjname(SubjectNameWrapper_Scheme * d_subjectNameWrapper_Scheme);
+__global__ void quicksort_scheme_paperid(PaperIdWrapper_Scheme * d_PaperIdWrapper_Scheme);
+__global__ void quicksort_results_paperid(PaperIdWrapper_Results * d_PaperIdWrapper_Results);
+__global__ void quicksort_results_rollnumber(RollNumberWrapper_Results * d_results_rollnumberWrapper_Results);
+__global__ void quicksort_results_name(NameWrapper_Results * d_nameWrapper_Results);
